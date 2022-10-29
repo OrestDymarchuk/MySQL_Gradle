764e976a91aef42b2ae78abfc6d55b1a47540687
Orest Dymarchuk <oliverkromvel2016@gmail.com>
2022-10-29 22:11:48 +0400

    DatabaseInitService is fixed and works, but it has SQLException during the compiling

--- app/src/main/java/mysql_gradle/data_base_services/DatabaseInitService.java
+++ app/src/main/java/mysql_gradle/data_base_services/DatabaseInitService.java
@@ -3,24 +3,39 @@ package mysql_gradle.data_base_services;
 import mysql_gradle.data_base.DataBase;
 import mysql_gradle.preferences.Preferences;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DatabaseInitService {
 
-    public void initDB(DataBase dataBase){
+    public void initDB(DataBase dataBase) {
         try {
-            String initDbFilename = new Preferences().getString(Preferences.INIT_DB_SQL_PATH);
+            BufferedReader reader = new BufferedReader(
+                    new FileReader(new Preferences().
+                            getString(Preferences.INIT_DB_SQL_PATH)));
 
-            String sql = String.join(
-                    "\n",
-                    Files.readAllLines(Paths.get(initDbFilename))
-            );
+            StringBuilder stringBuilder = new StringBuilder();
+            String s = null;
+            List<String> collect = new ArrayList<>();
 
-            System.out.println(sql);
+            while ((s = reader.readLine()) != null) {
+                collect = Arrays.stream(
+                                stringBuilder.
+                                        append(s).
+                                        append("\n").
+                                        toString().
+                                        split(";"))
+                        .toList();
+            }
 
-            dataBase.executeUpdate(sql);
+            for (String sql : collect) {
+                dataBase.executeUpdate(sql);
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
--- sql/init_db.sql
+++ sql/init_db.sql
@@ -1,14 +1,10 @@
 CREATE TABLE IF NOT EXISTS worker(
      id INT AUTO_INCREMENT PRIMARY KEY,
-
      name VARCHAR(1000) CONSTRAINT worker_name_ck CHECK
      (LENGTH(name) >= 2) NOT NULL,
-
      birthday DATE CONSTRAINT worker_birthdate_ck CHECK
      (birthday > '1900-01-01'),
-
      level VARCHAR(20) CONSTRAINT level_ck CHECK
-
      (level IN ('Trainee', 'Junior', 'Middle', 'Senior')) NOT NULL,
      salary INT CONSTRAINT worker_salary_ck CHECK
      (salary >= 100 AND salary <= 100000)
