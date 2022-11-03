78fae58474a18a62d4d667a2f16d7551e268be30
Orest Dymarchuk <oliverkromvel2016@gmail.com>
2022-11-04 01:35:24 +0400

    Added constants classes

---
+++ app/src/main/java/mysql_gradle/constants_classes/DataBasePopulateConstants.java
@@ -0,0 +1,31 @@
+package mysql_gradle.constants_classes;
+
+public class DataBasePopulateConstants {
+    public static final String TRAINEE_LEVEL = "Trainee";
+    public static final String JUNIOR_LEVEL = "Junior";
+    public static final String MIDDLE_LEVEL = "Middle";
+    public static final String SENIOR_LEVEL = "Senior";
+
+    public static final String PETER_CLIENT_NAME = "Peter";
+    public static final String LOIS_CLIENT_NAME = "Lois";
+    public static final String Brian_CLIENT_NAME = "Brian";
+    public static final String STEWIE_CLIENT_NAME = "Stewie";
+    public static final String CHRIS_CLIENT_NAME = "Chris";
+    public static final String MEG_CLIENT_NAME = "Meg";
+    public static final int CLIENT_ID_ONE = 1;
+    public static final int CLIENT_ID_TWO = 2;
+    public static final int CLIENT_ID_THREE = 3;
+    public static final int CLIENT_ID_FOUR = 4;
+    public static final int CLIENT_ID_FIVE = 5;
+    public static final int CLIENT_ID_SIX = 6;
+    public static final int PROJECT_ID_ONE = 1;
+    public static final int PROJECT_ID_TWO = 2;
+    public static final int PROJECT_ID_THREE = 3;
+    public static final int PROJECT_ID_FOUR = 4;
+    public static final int PROJECT_ID_FIVE = 5;
+    public static final int PROJECT_ID_SIX = 6;
+    public static final int PROJECT_ID_SEVEN = 7;
+    public static final int PROJECT_ID_EIGHT = 8;
+    public static final int PROJECT_ID_NINE = 9;
+    public static final int PROJECT_ID_TEN = 10;
+}
